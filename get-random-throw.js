24827640863b6cab744ad0af5d2f81da8b72d876
Libby Hamlin <bravolibby@gmail.com>
2020-04-06 15:29:58 -0700

    created ramdom number function and exported it.

---
+++ get-random-throw.js
@@ -0,0 +1,15 @@
+//function needs to generate random numbers and triage that into returning the strings rock, paper, or scissors accordingly 
+
+const getRandomThrow = () => {
+    const randomNumber = (Math.random() * 3) + 1;
+
+    if (randomNumber === 1) {
+        return 'rock';
+    } else if (randomNumber === 2) {
+        return 'paper';
+    } else if (randomNumber === 3) {
+        return 'scissors';
+    }
+};
+
+export default getRandomThrow;
