c72bb0390ba4795f10897dc6cc8d575a9324e399
Libby Hamlin <bravolibby@gmail.com>
2020-04-06 21:11:41 -0700

    made progress. Getting close.

---
+++ dev-ed-version.js
@@ -0,0 +1,46 @@
+// //import relevant functions
+// import checkResult from './check-result.js';
+// // import getRandomThrow from './get-random-throw.js';
+
+// //Create a whole function for the entire game, but also create separate ones that serve specific actions.
+// //Scoping everything inside this function to avoid global variables.
+
+// const game = () => {
+//     let pScore = 0;
+//     let cScore = 0;
+
+//     //Rather than adding all the elements up top I'm restraining myself to create all the variables inside the functions where they are needed.
+//     //There's no point to have this function above.
+//     const startGame = () => {
+//         const playBtn = document.querySelector('.intro-area button');
+//         const introScreen = document.querySelector('.intro-area');
+//         const match = document.querySelector('.match');
+
+//         //Everytime we click we will run an action
+//         playBtn.addEventListener('click', () => {
+//             introScreen.classList.add('fadeout');
+//             match.classList.add('fadeIn');
+//         });
+//     };
+
+//     //Play match
+//     Const playMatch = () => {
+//         const options = document.querySelectorAll('.option-area button');
+//         const playerHand = document.querySelector('.player-hand');
+//         const computerHand = document.querySelector('.computer-hand');
+//         const hands = document.querySelector('.hands img');
+
+//         hands.forEach(hand => {
+//             hand.addEventListener('animationend', function() {
+//                 this.style.animation = '';
+//             });
+//    });
+
+
+//     //Call all the inner function (Executes all the small code).
+//     startGame();
+//     playMatch();
+// };
+
+// //Start the game function (This were you execute the big one.)
+// game()};
--- r-p-s.js
+++ r-p-s.js
@@ -1,46 +1,91 @@
-//import relevant functions
-import checkResult from './check-result.js';
-// import getRandomThrow from './get-random-throw.js';
-
-//Create a whole function for the entire game, but also create separate ones that serve specific actions.
-//Scoping everything inside this function to avoid global variables.
-
 const game = () => {
     let pScore = 0;
-    let cScore = 0;
+    let cScore = 0; 
 
-    //Rather than adding all the elements up top I'm restraining myself to create all the variables inside the functions where they are needed.
-    //There's no point to have this function above.
+//start the game
     const startGame = () => {
-        const playBtn = document.querySelector('.intro-area button');
-        const introScreen = document.querySelector('.intro-area');
+        const playBtn = document.querySelector('.intro button'); 
+        const introScreen = document.querySelector('.intro');
         const match = document.querySelector('.match');
 
-        //Everytime we click we will run an action
         playBtn.addEventListener('click', () => {
-            introScreen.classList.add('fadeout');
+            introScreen.classList.add('fadeOut');
             match.classList.add('fadeIn');
+            // console.log('startGame');
         });
     };
 
     //Play match
-    Const playMatch = () => {
-        const options = document.querySelectorAll('.option-area button');
-        const playerHand = document.querySelector('.player-hand');
-        const computerHand = document.querySelector('.computer-hand');
-        const hands = document.querySelector('.hands img');
-
+    const playMatch = () => {
+        const options = document.querySelectorAll('.options button');
+        const playerHand = document.querySelector('player-hand');
+        const computerHand = document.querySelector('computer-hand');
+        const hands = document.querySelectorAll('.hands img');
+        
         hands.forEach(hand => {
             hand.addEventListener('animationend', function() {
                 this.style.animation = '';
             });
-   });
+        });
+        
+        //Computer options
+        const computerOptions = ['rock', 'paper', 'scissors'];
+
+        options.forEach(option => {
+            option.addEventListener('click', function() {
+               
+            //Computer choice    
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoice = computerOptions[computerNumber];
+
+                //Here is where we call compareHands/checkResult
+                compareHands(this.textContent, computerChoice);
+                //Update images
+                playerHands.src = `./assets/${this.textContent}.png`;
+                computerHand.src = `./assets/${computerChoice}.png`;
+                
+            });
+        });
+    };       
+    
+    const compareHands = (playerChoice, computerChoice) => {
+      //Update text
+        const winner = document.querySelector('.winner');
+        //Check for a tie
+        if (playerChoice === computerChoice) {
+            winner.textContent = 'It is a tie.';
+            return;
+        }
+
+        //check for rock
+        if (playerChoice === 'rock') {
+            if(computerChoice === 'scissors') {
+                winner.textContent = 'Player Wins';
+            } else {
+                winner.textContent = 'Computer Wins';
+                return;
+            }
+        }
+
+        //check for paper
+        if (playerChoice === 'paper') {
+            if(computerChoice === 'scissors') {
+                winner.textContent = 'Computer Wins';
+            } else {
+                winner.textContent = 'Player Wins';
+                return;
+
+        //check for scissors
+        
+    };
 
 
-    //Call all the inner function (Executes all the small code).
+    //Is call all the inner function
     startGame();
     playMatch();
 };
+    
 
-//Start the game function (This were you execute the big one.)
-game()};
+
+//start the game function
+game();
